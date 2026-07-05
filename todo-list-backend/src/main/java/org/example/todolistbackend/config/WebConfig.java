689b20d9f645a1dbf85bb0e40bdce30cf2ba9890
ThanhLy1603 <Lynguyenthanh1603@gmail.com>
2026-07-05 07:36:56 +0700

    Test

--- todo-list-backend/src/main/java/org/example/todolistbackend/config/WebConfig.java
+++ todo-list-backend/src/main/java/org/example/todolistbackend/config/WebConfig.java
@@ -7,8 +7,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // Cho phép tất cả các API bắt đầu bằng /api
-                .allowedOrigins("http://localhost:5173", "http://localhost:3000") // URL của Frontend
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
